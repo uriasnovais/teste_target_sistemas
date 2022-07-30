c0afc7e875b8f493cc3925c0f6747356ec4ba4a8
UriasNovais <j.uriasnovais@gmail.com>
2022-07-29 23:41:29 -0300

    qt 2 func fibo

--- qt_2.py
+++ qt_2.py
@@ -1,17 +1,18 @@
-numero_entrada = int(input("Insira um número: "))
-fim = 0
+# Gerar números da sequencia de fibonnaci até que seja gerado um número maior que o numero da entrada
+
 
 def sequencia_fibonnaci(fim):
     fibonnaci = [0, 1]
     count = 0
     while count < fim:
         fibonnaci.append(fibonnaci[count]+fibonnaci[count+1])
+        count += 1
     return fibonnaci
 
-gerador_fibonnaci = sequencia_fibonnaci(5)
+gerador_fibonnaci = sequencia_fibonnaci(10)
 print(gerador_fibonnaci)
 
-#while True:
-#    while numero_entrada < fibonnaci[-1]:
-#        fibonnaci.append()
-### Gerar números da sequencia de fibonnaci até que seja gerado um número maior que o numero entrada
+while True:
+    while numero_entrada < fibonnaci[-1]:
+        fibonnaci.append()
+
