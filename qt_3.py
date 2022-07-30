f0e46a1f14828ab6a4d562b4d07d9f285538c9aa
UriasNovais <j.uriasnovais@gmail.com>
2022-07-30 02:19:25 -0300

    qt 3 correcao

--- qt_3.py
+++ qt_3.py
@@ -6,10 +6,10 @@ import pandas as pd
 dados_1 = open('dados_1.json', 'r').read()  # Read data
 
 df_dados_1 = pd.read_json(dados_1)
-df_dados_1_media_mes = df_dados_1.mean()
+df_dados_1_media_mes = df_dados_1.query('`valor` > 0').mean()
 
-print(f"O Valor minimo registrado foi {df_dados_1.query('`valor` > 0').min()['valor']}"
-      f"\nO Valor máximo registrado foi {df_dados_1['valor'].max()};")
+print(f"O Faturamento minimo registrado diferente de 0 foi R$ {df_dados_1.query('`valor` > 0').min()['valor']}"
+      f"\nO Faturamento máximo registrado foi R$ {df_dados_1['valor'].max()};")
 
 query = df_dados_1.query(f"`valor` >= {df_dados_1_media_mes['valor']}")
 total_dias_acima_media = 0
@@ -17,9 +17,7 @@ total_dias_acima_media = 0
 for d in query['dia']:
     total_dias_acima_media += 1
 
-print(f"O total de dias nos quais o valor é superior a média, {df_dados_1_media_mes['valor']:.4f} são {total_dias_acima_media};")
+print(f"O total de dias nos quais o Faturamento é superior a média, R$ {df_dados_1_media_mes['valor']:.4f}, são {total_dias_acima_media};")
 
 #Printar a query mostrando somente os dias superiores a média
 #print(query)
-
-
