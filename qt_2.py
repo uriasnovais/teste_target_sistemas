28ac41eca7dd70525b47d48aaaa21fd9d63fa50b
UriasNovais <j.uriasnovais@gmail.com>
2022-07-29 23:26:20 -0300

    qt 1

---
+++ qt_2.py
@@ -0,0 +1,12 @@
+fibonnaci = [0, 1]
+numero_entrada = int(input("Insira um número: "))
+fim = 0
+def sequencia_fibonnaci(fim):
+
+
+while True:
+    while numero_entrada < fibonnaci[-1]:
+        fibonnaci.append()
+
+
+### Gerar números da sequencia de fibonnaci até que seja gerado um número maior que o numero entrada
